9f3e7cca7d904bbd532ef821e8985471e220f4a3
Alexandru Coca <alexcoca23@yahoo.co.uk>
2022-03-23 10:18:54 +0000

    feat: add parsing and scoring wrapper compatible with hierarchical dir structure containing split, schema variant and model input data version

---
+++ parse_and_score_checkpoints_sgd_x.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+declare -a sgd_step=(74000)
+declare -a schema_variants=("original" "v1" "v2" "v3" "v4" "v5")
+
+if [ -z ${EXPERIMENT+x} ]; then
+  echo "Please pass the experiment name to the evaluation command by prepending EXPERIMENT=my_experiment_name variable."
+  exit
+fi
+
+if [ -z ${SPLIT+x} ]; then
+  echo "Please pass the split to the evaluation command by prepending SPLIT=my_split_name variable. The split name
+  should be one of 'dev', 'dev_small' or 'test'."
+  exit
+fi
+
+if [ -z ${VERSION+x} ]; then
+  echo "Please pass an integer representing the data version to the evaluation command. For example prepend VERSION_1 to
+  test data preprocessed in folder */version_1/"
+  exit
+fi
+
+
+HYPS_BASE_DIR=hyps
+echo "Base directory where hypotheses are found is $HYPS_BASE_DIR"
+echo "Converting decoder output to SGD format ..."
+for step in "${sgd_step[@]}"; do
+  echo "Parsing predictions of model at step $step"
+  for variant in "${schema_variants[@]}"; do
+    echo "Parsing schema variant: $variant"
+    python -m scripts.parse \
+      -d "$HYPS_BASE_DIR"/"$EXPERIMENT"/"$variant"/"$SPLIT"/version_"$VERSION"/model."$step" \
+      -s /scratches/neuron/dev/robust_paraphrases/dstc8-schema-guided-dialogue/sgd_x/data/"$variant"/"$SPLIT"/schema.json \
+      -t data/interim/blank_dialogue_templates/"$variant"/"$SPLIT" \
+      -j data/preprocessed/"$variant"/"$SPLIT"/version_"$VERSION"/data.json
+    mkdir -p "metrics/$EXPERIMENT/$variant/$SPLIT/version_$VERSION"
+  done
+done
+echo "Scoring hypotheses..."
+for step in "${sgd_step[@]}"; do
+    for variant in "${schema_variants[@]}"; do
+      echo "Scoring model at step $step, schema variant $variant"
+      python -m scripts.score \
+        --prediction_dir "$HYPS_BASE_DIR"/"$EXPERIMENT"/"$variant"/"$SPLIT"/version_"$VERSION"/model."$step" \
+        --raw_data_dir /scratches/neuron/dev/robust_paraphrases/dstc8-schema-guided-dialogue/sgd_x/data/"$variant" \
+        --eval_set "$SPLIT" \
+        --output_metric_file metrics/"$EXPERIMENT"/"$variant"/"$SPLIT"/version_"$VERSION"/model_"$step"_metrics.json
+    done
+done
