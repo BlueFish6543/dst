6265c83f18ac67fdda651c42643612744318068a
Alexandru Coca <alexcoca23@yahoo.co.uk>
2022-03-25 17:28:43 +0000

    feat: use helper from score_utils module

--- scripts/score.py
+++ scripts/score.py
@@ -20,7 +20,6 @@ from __future__ import division
 from __future__ import print_function
 
 import collections
-import glob
 import json
 import os
 
@@ -28,6 +27,7 @@ import numpy as np
 from absl import app, logging, flags
 
 import src.dst.metrics as metrics
+from dst.scoring_utils import get_dataset_as_dict
 
 FLAGS = flags.FLAGS
 
@@ -77,27 +77,6 @@ def get_service_set(schema_path):
 def get_in_domain_services(schema_path_1, schema_path_2):
     """Get the set of common services between two schemas."""
     return get_service_set(schema_path_1) & get_service_set(schema_path_2)
-
-
-def get_dataset_as_dict(file_path_patterns):
-    """Read the DSTC8 json dialog data as dictionary with dialog ID as keys."""
-    dataset_dict = {}
-    if isinstance(file_path_patterns, list):
-        list_fp = file_path_patterns
-    else:
-        list_fp = sorted(glob.glob(file_path_patterns))
-    for fp in list_fp:
-        if PER_FRAME_OUTPUT_FILENAME in fp or 'belief' in fp:
-            continue
-        logging.info("Loading file: %s", fp)
-        with open(fp, 'r') as f:
-            data = json.load(f)
-            if isinstance(data, list):
-                for dial in data:
-                    dataset_dict[dial["dialogue_id"]] = dial
-            elif isinstance(data, dict):
-                dataset_dict.update(data)
-    return dataset_dict
 
 
 def get_metrics(dataset_ref, dataset_hyp, service_schemas, in_domain_services):
