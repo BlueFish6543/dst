ca6e973a7ec7f7a03a6258ec8a2f7e0775f5145b
Alexandru Coca <alexcoca23@yahoo.co.uk>
2022-03-30 11:35:29 +0100

    change: refactor code to prepare for parsing without context but with a different target slot index separator

--- src/dst/parser.py
+++ src/dst/parser.py
@@ -129,9 +129,19 @@ def restore_case(value: str, service: str, restore_categorical_case: bool = True
             return recased_data[service]
 
 
-def parse_predicted_string(dialogue_id: str, turn_index: str, service: str, predicted_str: str, slot_mapping: dict,
-                           cat_values_mapping: dict, intent_mapping: dict, context: str,
-                           value_separator: Optional[str] = None, restore_categorical_case: bool = False) -> dict:
+def parse_predicted_string(
+        dialogue_id: str,
+        turn_index: str,
+        service: str,
+        predicted_str: str,
+        slot_mapping: dict,
+        cat_values_mapping: dict,
+        intent_mapping: dict,
+        context: str,
+        value_separator: Optional[str] = None,
+        restore_categorical_case: bool = False,
+        target_slot_index_separator: Optional[str] = ":"
+) -> dict:
 
     state = {
         "slot_values": {},
@@ -148,82 +158,30 @@ def parse_predicted_string(dialogue_id: str, turn_index: str, service: str, pred
 
     # Parse slot values
     if match.group(1).strip():  # if the string is not empty
-        pattern = r"(?<!^)\s+(?=[0-9]+:)"
+        pattern = rf"(?<!^)\s+(?=[0-9]+{target_slot_index_separator})"
         if value_separator == " || ":
-            pattern = r"(?<!^)\s+(?=[0-9]+:)(?<!\|\| )"
+            pattern = fr"(?<!^)\s+(?=[0-9]+{target_slot_index_separator})(?<!\|\| )"
         elif value_separator is not None:
             logger.error(f"State splitting pattern undefined for value separator {value_separator}")
         substrings = re.compile(pattern).split(match.group(1).strip())
-        skip = 0
-        for i, pair in enumerate(substrings):
-            if skip > 0:
-                skip -= 1
-                continue
-            pair = pair.strip().split(":", 1)  # slot value pair
-            if len(pair) != 2:
-                # String was not in expected format
-                logger.warning(f"Could not extract slot values in {predicted_str} in {dialogue_id}_{turn_index}.")
-                continue
-            try:
-                slot = slot_mapping[pair[0].strip()]
-                if slot in cat_values_mapping:
-                    # Categorical
-                    success = False
-                    # Invert the mapping to get the categorical value
-                    for categorical_value, categorical_value_idx in cat_values_mapping[slot].items():
-                        if categorical_value_idx == pair[1].strip():
-                            recased_value = recase(value=categorical_value, service=service)
-                            assert isinstance(recased_value, str)
-                            state["slot_values"][slot] = [recased_value]
-                            success = True
-                            break
-                    if not success:
-                        logger.warning(
-                            f"Could not extract categorical value for slot {pair[0].strip()} in "
-                            f"{predicted_str} in {dialogue_id}_{turn_index}. "
-                            f"Values defined for this slot were {cat_values_mapping[slot]}"
-                        )
-                else:
-                    # Non-categorical
-                    # Check if the next slot could potentially be part of the current slot
-                    value = pair[1]
-                    if value_separator is not None and value_separator in value:
-                        value_list = value.split(value_separator)
-                        value_list = [v.strip() for v in value_list]
-                    else:
-                        value_list = [value.strip()]
-                    j = i + 1
-                    while j < len(substrings):
-                        # Check if the combined string exists in the context
-                        for idx, value in enumerate(value_list):
-                            if value_separator is None:
-                                possible_continuations = [substrings[j]]
-                            elif value_separator in substrings[j]:
-                                possible_continuations = substrings[j].split(value_separator)
-                            else:
-                                possible_continuations = [substrings[j]]
-                            for continuation in possible_continuations:
-                                # Replace spaces to avoid issues with whitespace
-                                if (value + continuation).replace(" ", "").lower() not in context.replace(" ", "").lower():
-                                    continue
-                                else:
-                                    value_list[idx] += " " + continuation
-                                    skip += 1
-                                    break
-                        else:
-                            break
-                    state["slot_values"][slot] = value_list
-                    for value in value_list:
-                        if value.replace(" ", "").lower() not in context.replace(" ", "").lower():
-                            # Replace spaces to avoid issues with whitespace
-                            if value.strip() not in SPECIAL_VALUES:
-                                logger.warning(
-                                    f"Predicted value {value.strip()} for slot {pair[0].strip()} "
-                                    f"not in context in {dialogue_id}_{turn_index}."
-                                )
-            except KeyError:
-                logger.warning(
-                    f"Could not extract slot {pair[0].strip()} in {predicted_str} in {dialogue_id}_{turn_index}.")
+        if target_slot_index_separator == ":":
+            # updates state  in-place
+            parse_with_context(
+                state,
+                substrings,
+                dialogue_id,
+                turn_index,
+                service,
+                predicted_str,
+                slot_mapping,
+                cat_values_mapping,
+                context,
+                value_separator=value_separator,
+                restore_categorical_case=restore_categorical_case,
+                target_slot_index_separator=target_slot_index_separator,
+            )
+        else:
+            raise ValueError(f"Unknown target slot index seprator {target_slot_index_separator}")
 
     # Parse intent
     intent = match.group(2).strip()
@@ -243,6 +201,104 @@ def parse_predicted_string(dialogue_id: str, turn_index: str, service: str, pred
                 f"Could not extract requested slot {index.strip()} in {predicted_str} in {dialogue_id}_{turn_index}.")
 
     return state
+
+
+def parse_with_context(
+        state: dict,
+        substrings: list[str],
+        dialogue_id: str,
+        turn_index: str,
+        service: str,
+        predicted_str: str,
+        slot_mapping: dict,
+        cat_values_mapping: dict,
+        context: str,
+        value_separator: Optional[str] = None,
+        restore_categorical_case: bool = False,
+        target_slot_index_separator: Optional[str] = ":"
+):
+    """Using ":" to separate slot indices from their values in the target sequence makes parsing difficult because this
+    is a symbol that appears in values. Therefore, 1:morning 11:15 is parsed as (1, morning), (11, 15) which is wrong.
+    This algorithm assumes the following substrings could be part of the current slot values and uses the context to
+    check whether this is the case. This algorithm is __NOT__ correct if the value separator is not `None`.
+    """
+
+    if value_separator is not None:
+        logger.warning(
+            "Parser algorithm is not correct for when multiple values are in the target sequence."
+            "Use at your own risk!"
+        )
+    recase = functools.partial(restore_case, restore_categorical_case=restore_categorical_case)
+    skip = 0
+    for i, pair in enumerate(substrings):
+        if skip > 0:
+            skip -= 1
+            continue
+        pair = pair.strip().split(f"{target_slot_index_separator}", 1)  # slot value pair
+        if len(pair) != 2:
+            # String was not in expected format
+            logger.warning(f"Could not extract slot values in {predicted_str} in {dialogue_id}_{turn_index}.")
+            continue
+        try:
+            slot = slot_mapping[pair[0].strip()]
+            if slot in cat_values_mapping:
+                # Categorical
+                success = False
+                # Invert the mapping to get the categorical value
+                for categorical_value, categorical_value_idx in cat_values_mapping[slot].items():
+                    if categorical_value_idx == pair[1].strip():
+                        recased_value = recase(value=categorical_value, service=service)
+                        assert isinstance(recased_value, str)
+                        state["slot_values"][slot] = [recased_value]
+                        success = True
+                        break
+                if not success:
+                    logger.warning(
+                        f"Could not extract categorical value for slot {pair[0].strip()} in "
+                        f"{predicted_str} in {dialogue_id}_{turn_index}. "
+                        f"Values defined for this slot were {cat_values_mapping[slot]}"
+                    )
+            else:
+                # Non-categorical
+                # Check if the next slot could potentially be part of the current slot
+                value = pair[1]
+                if value_separator is not None and value_separator in value:
+                    value_list = value.split(value_separator)
+                    value_list = [v.strip() for v in value_list]
+                else:
+                    value_list = [value.strip()]
+                j = i + 1
+                while j < len(substrings):
+                    # Check if the combined string exists in the context
+                    for idx, value in enumerate(value_list):
+                        if value_separator is None:
+                            possible_continuations = [substrings[j]]
+                        elif value_separator in substrings[j]:
+                            possible_continuations = substrings[j].split(value_separator)
+                        else:
+                            possible_continuations = [substrings[j]]
+                        for continuation in possible_continuations:
+                            # Replace spaces to avoid issues with whitespace
+                            if (value + continuation).replace(" ", "").lower() not in context.replace(" ", "").lower():
+                                continue
+                            else:
+                                value_list[idx] += " " + continuation
+                                skip += 1
+                                break
+                    else:
+                        break
+                state["slot_values"][slot] = value_list
+                for value in value_list:
+                    if value.replace(" ", "").lower() not in context.replace(" ", "").lower():
+                        # Replace spaces to avoid issues with whitespace
+                        if value.strip() not in SPECIAL_VALUES:
+                            logger.warning(
+                                f"Predicted value {value.strip()} for slot {pair[0].strip()} "
+                                f"not in context in {dialogue_id}_{turn_index}."
+                            )
+        except KeyError:
+            logger.warning(
+                f"Could not extract slot {pair[0].strip()} in {predicted_str} in {dialogue_id}_{turn_index}.")
 
 
 def populate_slots(
@@ -304,7 +360,9 @@ def populate_slots(
                                            ref_proc_turn["frames"][service_name]["cat_values_mapping"],
                                            ref_proc_turn["frames"][service_name]["intent_mapping"],
                                            context, value_separator=value_separator,
-                                           restore_categorical_case=restore_categorical_case)
+                                           restore_categorical_case=restore_categorical_case
+
+                                           )
             # Update
             empty_ref_frame_state = empty_ref_frame["state"]
             empty_ref_frame_state.update(state)
