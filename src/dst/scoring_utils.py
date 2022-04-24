98c88bbeecdfd5061df4567389f8263b52e2e68c
Alexandru Coca <alexcoca23@yahoo.co.uk>
2022-04-24 15:33:11 +0100

    fix: bug in load_hyps_and_refs call

--- src/dst/scoring_utils.py
+++ src/dst/scoring_utils.py
@@ -79,7 +79,9 @@ def setup_evaluator_inputs(
     A mapping containing the positional arguments of the official SGD evaluation script.
     """
     hyps_refs = load_hyps_and_refs(
-        hyp_path, inference_config.ref_path, decoded_only=inference_config.decode_only
+        hyp_path,
+        pathlib.Path(inference_config.ref_path),
+        decoded_only=inference_config.decode_only,
     )
     eval_schema_path = inference_config.ref_schema_path
     with open(eval_schema_path, "r") as f:
